4982a84d8de374d91f1cc2e3f25877855172545e
eneperezv <eneperezv@gmail.com>
2024-12-15 22:41:52 -0500

    Se incluye documentacion inicial de las clases
     Changes to be committed:
    	modified:   suggestify.api/scr/Controllers/AuthController.php
    	modified:   suggestify.api/scr/Controllers/RoutesController.php
    	modified:   suggestify.api/scr/Libs/load.php

--- suggestify.api/scr/Libs/load.php
+++ suggestify.api/scr/Libs/load.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * @(#)load.php 1.0 15/12/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones descritas en The MIT License (MIT)
+ * en https://opensource.org/license/mit
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 // LIBRERIAS
 require_once "src/Libs/firebase-php-jwt/JWTExceptionWithPayloadInterface.php";
 require_once "src/Libs/firebase-php-jwt/BeforeValidException.php";
@@ -13,18 +28,18 @@ require_once "src/Libs/firebase-php-jwt/SignatureInvalidException.php";
 // APP
 // CONTROLLERS
 require_once "src/Controllers/AuthController.php";
-require_once "src/Controllers/AuthorController.php";
-require_once "src/Controllers/PostController.php";
-require_once "src/Controllers/RoutesController.php";
+// require_once "src/Controllers/AuthorController.php";
+// require_once "src/Controllers/PostController.php";
+// require_once "src/Controllers/RoutesController.php";
 // DATA ACCESS
-require_once "src/DataAccess/AuthorDal.php";
-require_once "src/DataAccess/PostDal.php";
-require_once "src/DataAccess/UserDal.php";
+// require_once "src/DataAccess/AuthorDal.php";
+// require_once "src/DataAccess/PostDal.php";
+// require_once "src/DataAccess/UserDal.php";
 // MODELS
-require_once "src/Models/Author.php";
+// require_once "src/Models/Author.php";
 require_once "src/Models/Connection.php";
-require_once "src/Models/Post.php";
-require_once "src/Models/User.php";
+// require_once "src/Models/Post.php";
+// require_once "src/Models/User.php";
 // UTILS
 require_once "src/Utils/EnvLoader.php";
 require_once "src/Utils/Logger.php";
